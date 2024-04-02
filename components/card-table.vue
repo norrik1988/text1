6a4e80cfb90e66d7526813431df15c90bcbfaeac
vincenzomarino <vincenzomarino1988@hotmail.it>
2024-04-02 17:59:57 +0200

    2april2

--- components/card-table.vue
+++ components/card-table.vue
@@ -3,10 +3,20 @@
     <Card class="customCard h-18rem">
         <template #title><h4>Title card</h4>
         
-            <span class="p-input-icon-left">
+            <div class="min-h-screen flex flex-column relative flex-auto">
+        <div class="flex justify-content-between align-items-center px-5 surface-0 border-bottom-1 surface-border relative lg:static" style="height:60px">
+            <div class="flex">
+                <a v-ripple class="cursor-pointer block lg:hidden text-700 mr-3 mt-1 p-ripple"
+                    v-styleclass="{ selector: '#app-sidebar-1', enterClass: 'hidden', enterActiveClass: 'fadeinleft', leaveToClass: 'hidden', leaveActiveClass: 'fadeoutleft', hideOnOutsideClick: true }">
+                    <i class="pi pi-bars text-4xl"></i>
+                </a>
+                <span class="p-input-icon-left">
                     <i class="pi pi-search"></i>
                     <InputText type="text" class="border-none w-10rem sm:w-20rem" placeholder="Search" />
                 </span>
+            </div>
+        </div>
+        </div>
         </template>
         <template #content>
             <table1></table1>
@@ -50,6 +60,7 @@ p {
     padding: 0rem !important;
     gap: 1rem !important;
 }
+
 
 </style>
 
